da2a6338c37f425bd68c0d8c487fecd307c91e37
naveenaegis <naveenaegis@gmail.com>
2024-08-31 00:36:19 +0530

    Add: all the api of customer is added in the customer microservice

--- services/customer/src/main/java/com/naveen/ecommerce/customer/CustomerResponse.java
+++ services/customer/src/main/java/com/naveen/ecommerce/customer/CustomerResponse.java
@@ -1,13 +1,9 @@
 package com.naveen.ecommerce.customer;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
 public record CustomerResponse(
-    String id,
-    String firstname,
-    String lastname,
-    String email,
-    Address address) {
-
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address) {
 }
